6abdf7acaede82cf9858108c7f9f50273a813a4f
Dalia405 <daliamahmoud405@gmail.com>
2022-10-26 00:50:46 +0200

    Admin Login

---
+++ Luxury_Back/Controllers/Admin/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luxury_Back.Controllers.Admin
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Luxury_Back/Controllers/Admin/HomeAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luxury_Back.Controllers.Admin
+{
+    public class HomeAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Admin/HomeAdmin/idex.cshtml");
+        }
+    }
+}
