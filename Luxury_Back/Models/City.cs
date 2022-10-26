82f125348fdcc21dd2664b3dec58793a941c22c9
MohmedRady <75885710+MhmedRady@users.noreply.github.com>
2022-10-27 00:46:16 +0200

    add governorate_city_entity

---
+++ Luxury_Back/Models/City.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.Unicode;
+
+namespace Luxury_Back.Models
+{
+
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name_ar { get; set; }
+        public string name_en { get; set; }
+        [ForeignKey("Governorate")]
+        public int gov_id { get; set; }
+
+        public virtual Governorate Governorate { get; set; }
+
+    }
+}
