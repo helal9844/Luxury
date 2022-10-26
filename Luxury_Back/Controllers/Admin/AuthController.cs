45d6c1b6a96f30cd55a269a982e50c8e1963d1b2
MhmedRady <mhmed.rady.95@gmail.com>
2022-10-26 09:29:50 +0200

    update panel styles scripts layout

--- Luxury_Back/Controllers/Admin/AuthController.cs
+++ Luxury_Back/Controllers/Admin/AuthController.cs
@@ -4,9 +4,10 @@ namespace Luxury_Back.Controllers.Admin
 {
     public class AuthController : Controller
     {
+        const string ViewPath = "Views/Admin/Auth/";
         public IActionResult Login()
         {
-            return View();
+            return View($"{ViewPath}Login.cshtml");
         }
     }
 }
---
+++ Luxury_Back/Controllers/Admin/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luxury_Back.Controllers.Admin
+{
+    public class HomeController : Controller
+    {
+        const string ViewPath = "Views/Admin/Home/";
+        public IActionResult Index()
+        {
+            return View($"{ViewPath}Index.cshtml");
+        }
+    }
+}
