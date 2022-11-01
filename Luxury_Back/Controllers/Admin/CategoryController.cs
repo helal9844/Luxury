2de9442d2bfb17098ac9b7cdf978c208b2a6d2d1
Dalia405 <daliamahmoud405@gmail.com>
2022-11-01 20:29:24 +0200

    delete category

--- Luxury_Back/Controllers/Admin/CategoryController.cs
+++ Luxury_Back/Controllers/Admin/CategoryController.cs
@@ -2,6 +2,8 @@
 using Luxury_Back.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Diagnostics;
 
 namespace Luxury_Back.Controllers.Admin
 {
@@ -21,6 +23,36 @@ namespace Luxury_Back.Controllers.Admin
             var categories = luxuryDb.categories.Include(c => c.translations).Include(c=>c.parent).ThenInclude(p=>p.translations).ToList();
             return View($"{ViewPath}Index.cshtml", categories);
         }
-        
+        public IActionResult Delete(int id)
+        {
+            var category = luxuryDb.categories.Include(c=>c.childs).Where(i=>i.Id ==id).First();
+            if (category == null)
+            {
+                TempData["error_msg"] = "Category is Not Allow";
+            }
+            else if (category.childs.Count() != 0 && category.childs.Count != null)
+            {
+                TempData["error_msg"] = "Sorry Can't Remove This Category";
+            }
+            else
+            {
+                using(IDbContextTransaction transaction = luxuryDb.Database.BeginTransaction()) {
+                    try
+                    {
+                        luxuryDb.categories.Remove(category);
+                        luxuryDb.SaveChanges();
+                        transaction.Commit();
+                        TempData["success_msg"] = "Category Deleted Successfully!";
+                    }
+                    catch(Exception ex)
+                    {
+                        transaction.Rollback();
+                        TempData["error_msg"] = ex.Message;
+                    }
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
