2484ba72697b6b768b978d93d649d7b3a972e3d3
MohmedRady <75885710+MhmedRady@users.noreply.github.com>
2022-11-12 11:14:13 +0200

    user auth with login page

---
+++ Luxury_Back/Areas/Root/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Luxury_Back.Areas.Root.Controllers
+{
+    [Area("Root")]
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
