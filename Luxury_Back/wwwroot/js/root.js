2b692e3e0d71588983bc6c546e74789db24f3b9d
MohmedRady <75885710+MhmedRady@users.noreply.github.com>
2022-11-13 14:57:43 +0200

    sass & header

--- Luxury_Back/wwwroot/js/root.js
+++ Luxury_Back/wwwroot/js/root.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+console.log("root");
+
+$(".lang-item").on("click", function () {
+    let lang = $(this).data("lang");
+    console.log(lang);
+    $(".select_lang").val(lang);
+    console.log($(".select_lang"));
+    $(".lang-form").submit();
+});
