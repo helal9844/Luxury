8db2c1eb63fd5e028a6dfb4f33a2a9ac693a4b6d
sohail-Elbasel <sohailmohamed1994@gmail.com>
2022-11-19 03:46:12 +0200

    update description in ibooking

--- Luxury_Back/Areas/Root/Validations/UserRegisterValidation.cs
+++ Luxury_Back/Areas/Root/Validations/UserRegisterValidation.cs
@@ -15,23 +15,23 @@ namespace Luxury_Back
 
             RuleFor(user => user.username)
                .NotNull().WithMessage(x => localizer["username"] + localizer["nullError"])
-               .MinimumLength(8).WithMessage("Minimum Length can't be less than 8 characters");
+               .MinimumLength(8).WithMessage(localizer["pass_less_length"]);
 
             RuleFor(user => user.Email)
                 .NotNull().WithMessage(x => localizer["email"] + localizer["nullError"])
-                .EmailAddress().WithMessage("email not valid");
+                .EmailAddress().WithMessage(localizer["erroremail"]);
            // .Matches(@"^[A-Za-z0-9]+@([a-zA-Z]+\\.)+[a-zA-Z]{2,6}]&").WithMessage("email not valid please enter strong email");
           
             RuleFor(user => user.password)
                 .NotNull().WithMessage(x => localizer["password"] + localizer["nullError"])
-                .MinimumLength(8).WithMessage("Minimum Length can't be less than 8 characters");
+                .MinimumLength(8).WithMessage(localizer["pass_less_length"]);
             //.Matches("").WithMessage("please enter password strong");
 
             RuleFor(user => user).Custom((user, context) =>
             {
                 if (user.password != user.confirm_password)
                 {
-                    context.AddFailure(nameof(user.password), "Does Not Match With Password");
+                    context.AddFailure(nameof(user.password), localizer["pass_not_match"]);
                 }
             });
                 
